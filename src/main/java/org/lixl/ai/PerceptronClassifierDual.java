e97b06785568007b53e2525644c5aa3d08e1a74e
hp-win10-worker <nkiang@gmail.com>
2018-03-14 18:43:12 +0800

    add perceptron dual

--- src/main/java/org/lixl/ai/PerceptronClassifierDual.java
+++ src/main/java/org/lixl/ai/PerceptronClassifierDual.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Created by Administrator on 3/9/2018.
- *
- * 感知机算法——原始形式
+ * Created by Administrator on 3/14/2018.
+ * 感知机分类算法——对偶形式
  */
-public class PerceptronClassifier {
+public class PerceptronClassifierDual {
+
     //分类器参数
     private double[] w;         //权值向量组
     private double b = 0;       //偏置
     private double eta = 1;     // 步长/学习率  η
     ArrayList<Point> instanceList;
 
-    public PerceptronClassifier(ArrayList<Point> instanceList, double eta){
+    public PerceptronClassifierDual(ArrayList<Point> instanceList, double eta){
         this.instanceList = instanceList;
         w = new double[instanceList.get(0).x.length];
         this.eta = eta;
     }
-    public PerceptronClassifier(ArrayList<Point> arrayList){
+    public PerceptronClassifierDual(ArrayList<Point> arrayList){
         this.instanceList = arrayList;
         w = new double[arrayList.get(0).x.length];
         this.eta = 1;
@@ -87,6 +87,7 @@ public class PerceptronClassifier {
         b += eta * point.y;
     }
 
+
     private static void instance1(){
         //最终结果是  y = sign(1* x1 + 1* x2 - 3)
         Point p1 = new Point(new double[]{3, 3}, 1);
@@ -96,47 +97,12 @@ public class PerceptronClassifier {
         list.add(p1);
         list.add(p2);
         list.add(p3);
-        PerceptronClassifier classifier = new PerceptronClassifier(list);
-        classifier.Classify();
-    }
-    private static void instance2(){
-        //最终结果是  y = sign(1* x1 + 1* x2 - 3)
-        Point p1 = new Point(new double[]{0, 0, 0, 1}, -1);
-        Point p2 = new Point(new double[]{1, 0, 0, 0}, 1);
-        Point p3 = new Point(new double[]{2, 1, 0, 0}, 1);
-        Point p4 = new Point(new double[]{2, 1, 0, 1}, -1);
-
-        ArrayList<Point> list = new ArrayList<Point>();
-        list.add(p1);
-        list.add(p2);
-        list.add(p3);
-        list.add(p4);
-        PerceptronClassifier classifier = new PerceptronClassifier(list);
+        PerceptronClassifierDual classifier = new PerceptronClassifierDual(list);
         classifier.Classify();
     }
 
     public static void main(String[] args){
         instance1();
-        //instance2();
-    }
-
-}
-
-/**
- * 分类用。包含两部分：x表示输入R维空间向量，y表示分类值，只有-1和+1两类。
- */
-class Point {
-    //x表示 输入R维空间向量
-    double[] x = new double[2];
-    //y表示 分类值，只有-1和+1两类
-    double y = 0;
-
-    Point(double[] x, double y){
-        this.x = x;
-        this.y = y;
-    }
-    public Point(){
-
     }
 }
 
