54e218943651911a4d971956e8b0fd36bd24b784
hp-win10-worker <nkiang@gmail.com>
2018-04-17 17:18:37 +0800

    add webapp

---
+++ src/main/java/org/lixl/java8/Sth.java
@@ -0,0 +1,79 @@
+package org.lixl.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+/**
+ * Created by Administrator on 4/4/2018.
+ */
+public class Sth {
+
+    /*
+    public static void main(String[] args){
+
+        (new Sth()).doOptionals();
+        (new Sth()).doStreamsFilterSortedMap();
+    }
+
+    public void doConsumers(){
+        Consumer<Person> greeter = (p) -> System.out.println("Hello, " + p.firstName);
+        greeter.accept(new Person("Luke", "Skywalker"));
+    }
+
+    public void doComparators(){
+        Comparator<Person> comparator = (p1, p2) -> p1.firstName.compareTo(p2.firstName);
+        Person p1 = new Person("John", "Doe");
+        Person p2 = new Person("Alice", "Wonderland");
+
+        comparator.compare(p1, p2);     // > 0
+        comparator.reversed().compare(p1, p2);  // < 0
+    }
+
+    public void doOptionals(){
+        Optional<String> optional = Optional.of("bam");
+        optional.isPresent();       //true
+        optional.get();             //"bam"
+        optional.orElse("fallback");    //"bam"
+        optional.ifPresent((s) -> System.out.println(s.charAt(0)));     //"b"
+    }
+
+    public void doStreamsFilterSortedMap(){
+        List<String> stringCollection = new ArrayList<>();
+        stringCollection.add("ddd2");
+        stringCollection.add("aaa2");
+        stringCollection.add("bbb1");
+        stringCollection.add("aaa1");
+        stringCollection.add("bbb3");
+        stringCollection.add("ccc");
+        stringCollection.add("bbb2");
+        stringCollection.add("ddd1");
+
+        stringCollection.stream().filter((s) -> s.startsWith("a")).forEach(System.out::println);
+
+        stringCollection.stream().sorted().filter((s) -> s.startsWith("a")).forEach(System.out::println);
+        //sorted 只是创建一个流对象排序的视图，并不改变原来集合中元素的顺序
+        System.out.println(stringCollection);
+
+        stringCollection.stream().map(String::toUpperCase).sorted((a, b) -> b.compareTo(a)).forEach(System.out::println);
+    }*/
+
+}
+
+class Person {
+    String firstName;
+    String lastName;
+
+    Person() {}
+
+    Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+interface PersonFactory<P extends Person> {
+    P create(String firstName, String lastName);
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 4/16/2018
+  Time: 8:29 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+  世界你好22
+</body>
+</html>
