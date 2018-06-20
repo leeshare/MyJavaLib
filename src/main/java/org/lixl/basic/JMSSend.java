b8a35ca617153de589a16370243d1633cbe04554
hp-win10-worker <nkiang@gmail.com>
2018-06-20 08:46:15 +0800

    publish mode

---
+++ src/main/java/org/lixl/basic/JMSSend.java
@@ -0,0 +1,53 @@
+package org.lixl.basic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Administrator on 6/18/2018.
+ */
+@WebServlet("/Publish")
+public class JMSSend extends HttpServlet {
+    public static final long serialVersionID = 1L;
+
+    public JMSSend(){
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        try {
+            InitialContext ctx = new InitialContext();
+            Topic topic = (Topic)ctx.lookup("java:comp/env/topic/topic0");
+            TopicConnectionFactory connectionFactory = (TopicConnectionFactory)ctx.lookup("java:comp/env/topic/connectionFactory");
+            TopicConnection topicConnection = connectionFactory.createTopicConnection();
+            TopicSession topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            TopicPublisher topicPublisher = topicSession.createPublisher(topic);
+            topicPublisher.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            TextMessage message = topicSession.createTextMessage();
+            message.setText("Hello World");
+            topicPublisher.publish(message);
+
+            out.write("Message published: " + message.getText());
+
+            topicConnection.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/org/lixl/basic/JMSSubscriber.java
@@ -0,0 +1,49 @@
+package org.lixl.basic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Administrator on 6/18/2018.
+ */
+
+@WebServlet("/Subscribe")
+public class JMSSubscriber extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public JMSSubscriber(){
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        try {
+            InitialContext ctx = new InitialContext();
+            Topic topic = (Topic)ctx.lookup("java:/comp/env/topic/topic0");
+            TopicConnectionFactory connectionFactory = (TopicConnectionFactory)ctx.lookup("java:comp/env/topic/connectionFactory");
+            TopicConnection topicConnection = connectionFactory.createTopicConnection();
+            TopicSession topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            TopicSubscriber topicSubscriber = topicSession.createSubscriber(topic);
+            topicConnection.start();
+
+            TextMessage message = (TextMessage)topicSubscriber.receive();
+            out.write("Message received: " + message.getText());
+            topicConnection.close();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    protected  void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
