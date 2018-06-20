400f04fedd44ac53c08ba07b99b0d092ceb22b5c
lee <nkiang.li@gmail.com>
2018-06-20 08:31:32 +0800

    customer producer

---
+++ src/main/java/org/lixl/pattern/PCData.java
@@ -0,0 +1,28 @@
+package org.lixl.pattern;
+
+/**
+ * Created by lxl on 18/6/20.
+ */
+public class PCData {
+    private final int data;
+
+    public PCData(int data){
+        this.data = data;
+    }
+    public PCData(String d){
+        data = Integer.valueOf(d);
+    }
+
+    @Override
+    public String toString(){
+        return "data:" + data;
+    }
+
+    /*public void setData(int data){
+        this.data = data;
+    }*/
+    public int getData(){
+        return data;
+    }
+
+}
