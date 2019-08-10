52d2a5c5d996f21891dc82a94e57e655fdda9ce0
lee <nkiang.li@gmail.com>
2019-08-10 10:19:07 +0800

    sth

---
+++ src/test/java/B.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lxl on 19/8/9.
+ */
+public class B {
+    public static B t1 = new B();
+    public static B t2 = new B();
+    {
+        System.out.println("构造块");
+    }
+    static {
+        System.out.println("静态块");
+    }
+    public static void main(String[] args){
+        B t = new B();
+    }
+}
