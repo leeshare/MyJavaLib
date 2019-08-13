b188bb3659ed0e831838978c03780b1d82b3d8ff
hp-win10-worker <nkiang@gmail.com>
2019-08-13 18:10:07 +0800

    'sth'

--- src/test/java/B2.java
+++ src/test/java/B2.java
@@ -1,12 +1,13 @@
 /**
  * Created by Administrator on 8/13/2019.
- * 类加载顺序：
+ * 1. 类加载顺序：
  *      1)父类静态代码域（包括静态初始块，静态属性，但不包括静态方法）
         2)子类静态代码域（包括静态初始块，静态属性，但不包括静态方法）
         3)父类非静态代码域（包括非静态初始块，非静态属性）
         4)父类构造函数
         5)子类非静态代码域
         6)子类构造函数
+   2. 外部类 与 静态内部类 并无直接关系，只不过调用时 需要 B2.Sub  仅此而已
  */
 public class B2 {
     private String baseName = "_baseName";
@@ -31,5 +32,6 @@ public class B2 {
 
     public static void main(String[] args){
         B2 b = new Sub();
+
     }
 }
