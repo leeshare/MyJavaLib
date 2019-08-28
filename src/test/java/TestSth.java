d9c792b678584fa4ffe7c7c1b5174d35277abf14
hp-win10-worker <nkiang@gmail.com>
2019-08-28 11:31:12 +0800

    'sth

--- src/test/java/TestSth.java
+++ src/test/java/TestSth.java
@@ -59,6 +59,8 @@ public class TestSth {
 
         new TestSth().go();
 
+
+        System.out.println(getValue(2));
     }
 
     private static void assignment(){
@@ -108,11 +110,24 @@ public class TestSth {
     public void go(){
         Runnable r=new Runnable(){
             public void run(){
-                System.out.print("foo");
+                System.out.println("foo");
             }
         };
         Thread t=new Thread(r);
         t.start();
+    }
+
+    public static int getValue(int i) {
+        int result = 0;
+        switch (i) {
+            case 1:
+                result = result + i;
+            case 2:
+                result = result + i * 2;
+            case 3:
+                result = result + i * 3;
+        }
+        return result;
     }
 
 }
