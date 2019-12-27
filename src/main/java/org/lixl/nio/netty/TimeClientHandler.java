9ade1878c9daaec1028e6cdf8d4c71e066255005
nkiang <nkiang@gmail.com>
2019-12-27 14:48:33 +0800

    sth

--- src/main/java/org/lixl/nio/netty/TimeClientHandler.java
+++ src/main/java/org/lixl/nio/netty/TimeClientHandler.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.lixl.nio.nio.TimeClientHandle;
 
 import java.util.logging.Logger;
@@ -11,21 +12,23 @@ import java.util.logging.Logger;
 /**
  * Created by Administrator on 11/25/2019.
  */
-public class TimeClientHandler extends ChannelHandlerAdapter {
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
     private static final Logger logger = java.util.logging.Logger.getLogger(TimeClientHandle.class.getName());
     private final ByteBuf firstMessage;
 
     public TimeClientHandler() {
-        byte[] req = "QUERY TIME ORDER".getBytes();
+        byte[] req = "QUERY TIME ORDER [From Client]".getBytes();
         firstMessage = Unpooled.buffer(req.length);
         firstMessage.writeBytes(req);
     }
 
+    @Override
     public void channelActive(ChannelHandlerContext ctx) {
         ctx.writeAndFlush(firstMessage);
     }
 
-    public void chanelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
@@ -33,9 +36,16 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
         System.out.println("Now is : " + body);
     }
 
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         //释放资源
         logger.warning("Unexpected exception from downstream : " + cause.getMessage());
+        cause.printStackTrace();
         ctx.close();
     }
 
--- src/main/java/org/lixl/nio/netty/TimeServerHandler.java
+++ src/main/java/org/lixl/nio/netty/TimeServerHandler.java
@@ -4,32 +4,37 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.util.Date;
 
 /**
  * Created by Administrator on 11/25/2019.
  */
-public class TimeServerHandler extends ChannelHandlerAdapter {
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
 
+    @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
         String body = new String(req, "UTF-8");
         System.out.println("The time server receive order : " + body);
-        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
-                ? new Date(System.currentTimeMillis()).toString()
+        String currentTime = body.contains("QUERY TIME ORDER")
+                ? new Date(System.currentTimeMillis()).toString() + " [From Server]"
                 : "BAD ORDER";
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
         ctx.write(resp);
     }
 
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
         ctx.flush();
     }
 
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
         ctx.close();
     }
 }
